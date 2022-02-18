b5f17a745169400a71d1f8b382f4cd55facbc10c
Joseph Reynolds <joseph-reynolds@charter.net>
2022-02-18 14:34:43 -0600

    Handle faillock for unknown user (#9)
    This fixes a bug in how the user manager interprets the result of the
    command `faillock --user USER`.  Incorrect results were given for users
    who had not yet attempted to authenticate via Linux-PAM.
    Getting the the D-Bus property UserLockedForFailedAttempt would result
    in an error instead of returning values.
    
    Note this also messed up applications which are built on top of user
    management (such as BMCWeb Redfish and the Web application) which may
    fail to shown any user information.
    
    A possible workaround is to attempt to authenticate, at least one time,
    for all the users on the system.  For IBM systems, this may be root,
    admin, and service.
    
    Introduced by https://github.com/ibm-openbmc/phosphor-user-manager/
    commit/87a6ab71de880d5e28b95a2077cb9d2758e037d9 when faillock was added.
    
    Tested:
    With /redfish/v1/AccountService properties AccountLockoutThreshold and
    AccountLockoutDuration set to nonzero values:
    Created newuser and read UserLockedForFailedAttempt:
    ```
    > busctl call xyz.openbmc_project.User.Manager /xyz/openbmc_project/user \
    >   xyz.openbmc_project.User.Manager CreateUser sassb newuser 1 redfish \
    >   priv-admin true
    > busctl introspect xyz.openbmc_project.User.Manager \
    >   /xyz/openbmc_project/user/newuser
    ```
    
    Then attempted login as newuser with bad password multiple times, and
    watched the output of `faillock --user newuser` and the D-Bus property
    UserLockedForFailedAttempt turn from false to true.
    
    Signed-off-by: Joseph Reynolds <joseph-reynolds@charter.net>

--- user_mgr.cpp
+++ user_mgr.cpp
@@ -667,7 +667,10 @@ bool UserMgr::userLockedForFailedAttempt(const std::string& userName)
         elog<InternalFailure>();
     }
 
-    // Output is two header lines followed by zero or more records:
+    // Expected output:
+    // If user is not known to faillock, output is empty.
+    // If user is known to faillock, output is two header lines followed by zero
+    // or more records:
     // > {userName}:
     // > "When        Type    Source          Valid"
     // > ${timestamp} ${type} {RHOST,TTY,SVC} {V,I}
@@ -677,7 +680,13 @@ bool UserMgr::userLockedForFailedAttempt(const std::string& userName)
     // perror}
     // Example: /usr/sbin/faillock: Error opening the tally file for admin:Not
     // a directory
-    if (output.size() < 2)
+
+    int failedAttempts = 0;
+    if (output.empty())
+    {
+        failedAttempts = 0;
+    }
+    else if (output.size() < 2)
     {
         log<level::ERR>("faillock resulted in error",
                         entry("USER=%s", userName.c_str()));
@@ -688,8 +697,10 @@ bool UserMgr::userLockedForFailedAttempt(const std::string& userName)
         }
         elog<InternalFailure>();
     }
-
-    int failedAttempts = output.size() - 2;
+    else
+    {
+        failedAttempts = output.size() - 2;
+    }
     if (AccountPolicyIface::maxLoginAttemptBeforeLockout() != 0 &&
         failedAttempts >= AccountPolicyIface::maxLoginAttemptBeforeLockout())
     {
